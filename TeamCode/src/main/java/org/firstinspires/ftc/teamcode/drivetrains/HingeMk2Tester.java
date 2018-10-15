06726f1700423ad89ab9d5254031e87a9d0ce675
minerharry <miner.harry567@gmail.com>
2018-10-15 15:54:35 -0400

    Updated DogeCV

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrains/HingeMk2Tester.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.drivetrains;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Hinge Mark 2 Tester w/ Adjustability", group="Testing")
+public class HingeMk2Tester  extends OpMode {
+
+
+    @Override
+    public void init()
+    {
+
+    }
+    @Override
+    public void loop()
+    {
+
+    }
+
+}
