4b67092db2969616713a4a2319ce41986b01c30a
olivia-zimmerman <olivia_zimmerman@caryacademy.org>
2018-10-26 16:08:59 -0400

    added setDriveForTank in the class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/roverRuckus/RoverRuckusHardware.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.opmode.roverRuckus;
+
+public class RoverRuckusHardware extends RuckusRobotHardware {
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/roverRuckus/RuckusRobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/roverRuckus/RuckusRobotHardware.java
@@ -9,55 +9,56 @@ import org.firstinspires.ftc.teamcode.R;
 import java.util.ArrayList;
 
 public abstract class RuckusRobotHardware extends RobotHardware {
-    public enum RuckusMotorName{
-        DRIVE_FRONT_LEFT (getResourceString(R.string.backLeft)),
-        DRIVE_FRONT_RIGHT (getResourceString(R.string.backRight)),
-        DRIVE_BACK_LEFT (getResourceString(R.string.frontLeft)),
-        DRIVE_BACK_RIGHT (getResourceString(R.string.frontRight)),
+    public enum RuckusMotorName {
+        DRIVE_FRONT_LEFT(getResourceString(R.string.backLeft)),
+        DRIVE_FRONT_RIGHT(getResourceString(R.string.backRight)),
+        DRIVE_BACK_LEFT(getResourceString(R.string.frontLeft)),
+        DRIVE_BACK_RIGHT(getResourceString(R.string.frontRight)),
         WINCH_MAIN(getResourceString(R.string.winchMotorMain)),
         WINCH_ARM(getResourceString(R.string.winchMotorArm));
 
         private String myName;
         private MotorName myMotorName;
+
         RuckusMotorName(String name) {
             myName = name;
             myMotorName = new RobotHardware.MotorName(name);
         }
-        String getName()
-        {
+
+        String getName() {
             return myName;
         }
-        MotorName getMotorName()
-        {
+
+        MotorName getMotorName() {
             return myMotorName;
         }
 
     }
-    public enum RuckusServoName
-    {
+
+    public enum RuckusServoName {
         J("3");
         private String myName;
         private ServoName myServoName;
-        RuckusServoName(String name)
-        {
+
+        RuckusServoName(String name) {
             myName = name;
             myServoName = new RobotHardware.ServoName(name);
         }
-        String getName()
-        {
+
+        String getName() {
             return myName;
         }
-        ServoName getServoName()
-        {
+
+        ServoName getServoName() {
             return myServoName;
         }
     }
+
     @Override
     public ArrayList<MotorName> getMotors() {
         RuckusMotorName.
-        ArrayList<MotorName> names = new ArrayList<MotorName>();
-        for(RuckusRobotHardware.RuckusMotorName name : RuckusRobotHardware.RuckusMotorName.values())
-        {
+                ArrayList<MotorName> names = new ArrayList<MotorName>();
+        for (RuckusRobotHardware.RuckusMotorName name : RuckusRobotHardware.RuckusMotorName.values()) {
             names.add(name.getMotorName());
         }
         return names;
@@ -66,10 +67,16 @@ public abstract class RuckusRobotHardware extends RobotHardware {
     @Override
     public ArrayList<ServoName> getServos() {
         ArrayList<ServoName> names = new ArrayList<ServoName>();
-        for(RuckusRobotHardware.RuckusServoName name : RuckusRobotHardware.RuckusServoName.values())
-        {
+        for (RuckusRobotHardware.RuckusServoName name : RuckusRobotHardware.RuckusServoName.values()) {
             names.add(name.getServoName());
         }
         return names;
+    }
+
+    protected void setDriveForTank(double left, double right)
+    {setPower(RuckusMotorName.DRIVE_BACK_LEFT.getMotorName(), left);
+    setPower(RuckusMotorName.DRIVE_BACK_RIGHT.getMotorName(), right);
+    setPower(RuckusMotorName.DRIVE_FRONT_LEFT.getMotorName(), left);
+    setPower(RuckusMotorName.DRIVE_FRONT_RIGHT.getMotorName(), right);
     }
 }
